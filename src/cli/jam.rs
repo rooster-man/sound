bd579de84f60039054829ceef6751da8b6e17e0c
rooster man <roostermanred@gmail.com>
2025-08-05 15:28:50 -0500

    other keys for more octaves

--- src/cli/jam.rs
+++ src/cli/jam.rs
@@ -1,5 +1,4 @@
 use super::args::Args;
-use crate::audio::square_wave::SquareWave;
 use crate::music::key::Key;
 use crate::music::note::{MusicNote, Note};
 use crate::music::util::get_scale_by_name;
@@ -12,7 +11,7 @@ use crossterm::execute;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use rodio::{
     cpal::{traits::*, BufferSize, SupportedBufferSize},
-    source::{LimitSettings, SawtoothWave, SineWave},
+    source::{LimitSettings, SineWave},
     OutputStream, OutputStreamBuilder, Sink, Source,
 };
 use std::collections::HashMap;
@@ -23,6 +22,19 @@ pub fn jam(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
     let (scale_intervals, _scale_name) = get_scale_by_name(&args.scale)?;
 
     let stream_handle = build_stream_handle()?;
+
+    println!("\n🎹 Multi-Octave Jam Mode 🎹");
+    println!("Scale: {}", args.scale);
+    println!("Base key: {:?} (octave {})", key.root, key.octave);
+    println!("\nKeyboard Layout:");
+    println!("  Numbers 1-8:  Octave {} (base)", key.octave);
+    println!("  QWERTYUI:     Octave {} (+1)", key.octave + 1);
+    println!("  ASDFGHJK:     Octave {} (+2)", key.octave + 2);
+    println!("  ZXCVBNM,:     Octave {} (+3)", key.octave + 3);
+    println!("\nControls:");
+    println!("  ↑/↓ arrows: Change base octave");
+    println!("  Ctrl+C: Exit");
+    println!("\nPress and hold keys to play notes...\n");
 
     enable_raw_mode()?;
     let mut stdout = std::io::stdout();
@@ -46,23 +58,58 @@ pub fn jam(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
 
                     if key_event.code == KeyCode::Up {
                         key.octave += 1;
+                        println!("Base octave: {}", key.octave);
                     }
                     if key_event.code == KeyCode::Down {
                         key.octave -= 1;
+                        println!("Base octave: {}", key.octave);
                     }
 
                     if let KeyCode::Char(c) = key_event.code {
-                        if c.is_ascii_digit() && c >= '1' && c <= '8' {
-                            if !active_keys.contains_key(&key_id) {
-                                let scale_index = c.to_digit(10).unwrap() as usize - 1;
+                        let (octave_offset, scale_index_opt) = match c.to_ascii_lowercase() {
+                            '1'..='8' => (0, Some(c.to_digit(10).unwrap() as usize - 1)),
 
+                            'q' => (1, Some(0)),
+                            'w' => (1, Some(1)),
+                            'e' => (1, Some(2)),
+                            'r' => (1, Some(3)),
+                            't' => (1, Some(4)),
+                            'y' => (1, Some(5)),
+                            'u' => (1, Some(6)),
+                            'i' => (1, Some(7)),
+
+                            'a' => (2, Some(0)),
+                            's' => (2, Some(1)),
+                            'd' => (2, Some(2)),
+                            'f' => (2, Some(3)),
+                            'g' => (2, Some(4)),
+                            'h' => (2, Some(5)),
+                            'j' => (2, Some(6)),
+                            'k' => (2, Some(7)),
+
+                            'z' => (3, Some(0)),
+                            'x' => (3, Some(1)),
+                            'c' => (3, Some(2)),
+                            'v' => (3, Some(3)),
+                            'b' => (3, Some(4)),
+                            'n' => (3, Some(5)),
+                            'm' => (3, Some(6)),
+                            ',' => (3, Some(7)),
+
+                            _ => (0, None),
+                        };
+
+                        if let Some(scale_index) = scale_index_opt {
+                            if scale_index < scale_intervals.len()
+                                && !active_keys.contains_key(&key_id)
+                            {
+                                let note_key = Key::new(key.root, key.octave + octave_offset);
                                 let note = MusicNote::from_key_interval(
-                                    &key,
+                                    &note_key,
                                     scale_intervals[scale_index],
                                     Duration::from_secs(10),
                                 );
 
-                                let square_wave = SquareWave::from_note(&note, 44100);
                                 let sink = Sink::connect_new(&stream_handle.mixer());
                                 let sine_wave = SineWave::new(note.frequency());
 
@@ -75,6 +122,12 @@ pub fn jam(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
 
                                 sink.append(limited);
                                 active_keys.insert(key_id, sink);
+
+                                println!(
+                                    "Playing note {} (octave {})",
+                                    scale_index + 1,
+                                    note_key.octave
+                                );
                             }
                         }
                     }
